3ecce3e11ce7d22d9961d6e45e25f11a97425b85
sourcecode69 <84104171+sourcecode69@users.noreply.github.com>
2022-03-29 15:41:57 +0530

    Create stackcalss.cpp

---
+++ stackandqueue/stackcalss.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+class stack{
+    private:
+    int nextindex;
+    int *data;
+    int capacity;
+    
+    public:
+    stack(int totalsize){
+        int n =0;
+        data = new int[totalsize];
+        nextindex =0;
+        capacity = totalsize;
+    }
+public:
+    int size(){
+        return nextindex;
+    }
+    bool sempty(){
+        if(nextindex==0){
+            return true;
+        }
+        else{ 
+        false;
+            
+        }
+    }
+    void push(int n ){
+        if(nextindex == capacity){
+            cout<<"stack full"<<endl;
+            return;
+        }
+        else {
+        data[nextindex] = n ;
+        nextindex++;
+        }
+
+    }
+int pop(){
+    if(nextindex==0){
+        cout<<"stack empty";
+        return 0;
+    }
+    nextindex--;
+    return data[nextindex];
+}
+
+int top(){
+    if(nextindex ==0){
+        return 0 ;
+    }
+    else 
+    return data[nextindex-1];
+}
+
+};
+
+
+int main[]{
+    class stack s(4);
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+
+cout<<s.pop()<<endl;
+cout<<s.pop()<<endl;
+//cout<<s.pop()<<endl;
+cout<<s.pop()<<endl;
+cout<<s.top()<<endl;
+cout<<s.top()<<endl;
+s.push(20);
+cout<<s.top()<<endl;
+cout << s.size() <<endl;
+//cout<< s.sempty() <<endl;
+return  0;
+
+}
