beb9be6db5505a8276b4fff04175c77f77085e0a
shivam <84104171+sourcecode69@users.noreply.github.com>
2022-05-07 13:23:42 +0530

    Update staircasedp.cpp

--- dp-1/staircasedp.cpp
+++ dp-1/staircasedp.cpp
@@ -1,4 +1,4 @@
-  long ans[n+1];
+long staircasedp(int n){  long ans[n+1];
     ans[0]=0;
     ans[1]=1;
     ans[2] = 2;
@@ -7,3 +7,15 @@
         ans[i]= ans[i-1]+ans[i-2]+ans[i-3];
     }
     return ans[n];
+}
+//by recursion.
+int staircase(int n){
+   if( n==0||n==1 || n==2){
+        return n;
+    }
+    if(n==3){
+        return 4;
+    }
+    long ans = staircase(n-1) + staircase(n-2) + staircase(n-3);
+    return ans;
+}
