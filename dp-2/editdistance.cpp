15a013343f62c6bdd6048cdea4e564479a33f262
shivam <84104171+sourcecode69@users.noreply.github.com>
2022-05-14 15:27:22 +0530

    Update editdistance.cpp

--- dp-2/editdistance.cpp
+++ dp-2/editdistance.cpp
@@ -29,3 +29,69 @@ return  m;
     }
    
 }
+
+
+//memoisation 
+int help(string s1, string s2, int **output){
+       int m = s1.size();
+    int n = s2.size();
+    // base case
+    if(m==0 && n==0){
+        return 0;
+    }
+      if(m ==0 && n!=0){
+        output[m][n] = n;
+          return n;
+    }
+    if(n==0 && m!=0){
+output[m][n] = m;
+        return m;
+    }
+    if(output[m][n] != -1){
+        return output[m][n];
+    }
+    int x =0;
+   
+    //recursive call
+    if(s1[0] == s2[0]){
+       x =  help(s1.substr(1), s2.substr(1),output);
+    }
+    else{
+    int a = help(s1.substr(1), s2,output);
+        int b = help(s1,s2.substr(1),output);
+        int c = help(s1.substr(1), s2.substr(1),output);
+         x =  1+ min(a,min(b,c));
+      
+    }
+    output[m][n] = x;
+return x;
+}
+
+
+
+int editDistance(string s1, string s2)
+{
+	//Write your code here
+    int m = s1.size();
+    int n = s2.size();
+    int **output = new int *[m+1];
+    for(int i =0; i<=m; i++){
+
+    output[i] = new int[n+1];
+    }
+    for(int i =0; i<=m; i++){
+        for(int j =0; j<=n; j++){
+            output[i][j] = -1;
+        }
+    }
+    
+    return help(s1,s2,output);
+    
+}
+
+
+// dynamic programming.
+
+
+
+
